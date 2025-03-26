1d4a202a10dee56bdf3b1d55b2663b5a51f79a14
mxmarvel <okikifriyo@gmail.com>
2025-03-26 01:25:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'mx bot',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "mdx bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '9015540808', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
